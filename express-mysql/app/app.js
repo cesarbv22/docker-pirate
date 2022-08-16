381df4cd6c7c0e0566fde99c9d0598896978ae06
cesarbv22 <cesar.boza@grupobabel.com>
2022-08-16 11:56:19 -0600

    First Commit

--- express-mysql/app/app.js
+++ express-mysql/app/app.js
@@ -1,13 +1,17 @@
-const express = require("express");
+const express = require("express"); // utilización de dependencias que se instalan en el package.json
 const morgan = require("morgan");
 
-const app = express();
-const router = require("./routes/developers.js");
+const app = express();// declaración de la aplicación y forma de usar API
+const router = require("./routes/developers.js");// dependencia que es equivalente a un controlador, permite redireccionar cuando
+//alguien haga los request y teniendo en cuenta que se redireccione a servicios, lógica de negocios o así. Es una importación de 
+//dependencias relativa, por el .
 
+//configuración del puerto. Cuando tenga || quiere decir que está tratando de acceder a la variable de entorno o de lo contrario
+//el puerto 3000 
 const PORT = process.env.APP_PORT || 3000;
 
 app.disable("etag"); // Disable cache 304 status-code
-app.use(morgan("short")); // Log server requests
+app.use(morgan("short")); // Log server requests, esto es para hacer monitoreo de las apis 
 app.use(express.json()); // Parse JSON bodies (legacy was body-parser)
 app.use(router); // Expand routes functionalities with custom router
 
@@ -15,6 +19,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello, My name is Santiago." });
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // aquí se le dice a la aplicación, el puerto por el que va a 
+//funcionar 
 
 module.exports = app;
